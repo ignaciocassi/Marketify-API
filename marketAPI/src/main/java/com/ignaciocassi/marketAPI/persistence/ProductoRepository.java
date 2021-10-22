7006e62c31d64e02fc29b0ceb4eefbbfd8fb60cc
Nacho <nacho.cassi97@gmail.com>
2021-10-22 18:18:41 -0300

    -Creados CategoryMapper y ProductMapper, los cuales mapean Productos (DAO) <-> Product (DTO), Categoria (DAO) <-> Category (DTO) respectivamente, mediante MapStruct, aplicando así el patrón Data Mapper. -Sigiendo el patron DDD, se orientó al dominio el ProductoRepository al implementar la interface ProductRepository para que este trabaje en términos de Product (DTO), utilizando el ProductMapper localmente como "mapper".

--- marketAPI/src/main/java/com/ignaciocassi/marketAPI/persistence/ProductoRepository.java
+++ marketAPI/src/main/java/com/ignaciocassi/marketAPI/persistence/ProductoRepository.java
@@ -1,51 +1,68 @@
 package com.ignaciocassi.marketAPI.persistence;
 
+import com.ignaciocassi.marketAPI.domain.Product;
+import com.ignaciocassi.marketAPI.domain.repository.ProductRepository;
 import com.ignaciocassi.marketAPI.persistence.crud.ProductoCrudRepository;
 import com.ignaciocassi.marketAPI.persistence.entities.Producto;
+import com.ignaciocassi.marketAPI.persistence.mapper.ProductMapper;
 import org.springframework.stereotype.Repository;
-
 import java.util.List;
 import java.util.Optional;
 
 @Repository
-public class ProductoRepository {
+public class ProductoRepository implements ProductRepository {
 
+    //Para orientar el ProductoRepository al dominio, debemos implementar la interface ProductRepository.
     //Se encarga de interactuar con la base de datos.
-
     //Usa el productoCrudRepository, el cual define el comportamiento personalizado, además del CRUD proveído
-    // por el repository CrudRepository, del cual hereda.
+    //por el repository CrudRepository, del cual hereda.
+    //Adicionalmente utiliza ProductMapper para convertir de Producto a Product y viceversa.
     private ProductoCrudRepository productoCrudRepository;
+    private ProductMapper mapper;
 
-    public List<Producto> getAll() {
-        return (List<Producto>) productoCrudRepository.findAll();
+    @Override
+    public List<Product> getAll() {
+        //Obtiene la lista de Productos de la base de datos, y la convierte a lista de Product mediante mapper.
+        List<Producto> productos = (List<Producto>) productoCrudRepository.findAll();
+        return mapper.toProducts(productos);
     }
 
-    public List<Producto> getByCategoria(Integer idCategoria) {
-        return productoCrudRepository.findByIdCategoriaOrderByNombreAsc(idCategoria);
+    @Override
+    public Optional<List<Product>> getByCategory(int categoryId) {
+        List<Producto> productos = productoCrudRepository.findByIdCategoriaOrderByNombreAsc(categoryId);
+        return Optional.of(mapper.toProducts(productos));
     }
 
-    public Optional<List<Producto>> getEscasos(int cantidad) {
-        return productoCrudRepository.findByCantidadStockLessThanAndEstado(cantidad, true);
+    @Override
+    public Optional<List<Product>> getScarceProducts(int quantity) {
+        Optional<List<Producto>> productos = productoCrudRepository.findByCantidadStockLessThanAndEstado(quantity,true);
+        return productos.map(prods -> mapper.toProducts(prods));
     }
 
-    public Optional<Producto> getProductoById(int id) {
-        return productoCrudRepository.findById(id);
+    @Override
+    public Optional<Product> getProduct(int productId) {
+        return productoCrudRepository.findById(productId).map(producto -> mapper.toProduct(producto));
     }
 
-    public Optional<Producto> getProductoByNombre(String nombre) {
-        return productoCrudRepository.findByNombre(nombre);
+    @Override
+    public Optional<Product> getProductByName(String name) {
+        return productoCrudRepository.findByNombre(name).map(producto -> mapper.toProduct(producto));
     }
 
-    public Producto save(Producto producto) {
-        return productoCrudRepository.save(producto);
+    @Override
+    public Product save(Product product) {
+        Producto producto = mapper.toProducto(product);
+        return mapper.toProduct(productoCrudRepository.save(producto));
     }
 
-    public void delete(Integer idProducto) {
-        productoCrudRepository.deleteById(idProducto);
+    @Override
+    public void delete(int productId) {
+        productoCrudRepository.deleteById(productId);
     }
 
-    public void delete(String nombre) {
-        productoCrudRepository.deleteByNombre(nombre);
+    @Override
+    public void delete(String name) {
+        productoCrudRepository.deleteByNombre(name);
     }
 
 }
